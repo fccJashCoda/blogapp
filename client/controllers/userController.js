724f996d35a2268a87ee6ce706c960c27dc7fe89
fccJashCoda <personalrevolution2014@gmail.com>
2020-08-02 23:09:11 +0200

    protected route to user account

--- client/controllers/userController.js
+++ client/controllers/userController.js
@@ -1,3 +1,21 @@
+const User = require('../models/user');
+
 exports.get_user_profile = (req, res) => {
-  res.render("userpage");
+  res.render('userprofile');
+};
+
+exports.get_user_account = async (req, res) => {
+  try {
+    const getUser = await User.findOne({ username: req.params.username });
+    if (String(res.locals.user.id) !== String(getUser._id)) {
+      return res.render('404', { status: '403', msg: ' - Access Denied' });
+    }
+    return res.render('useraccount');
+  } catch (error) {
+    return res.render('404', { status: '500', msg: 'Server error' });
+  }
+};
+
+exports.post_user_account = (req, res) => {
+  res.json({ msg: 'settings updated' });
 };
--- client/routes/user.js
+++ client/routes/user.js
@@ -1,8 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController");
-const { checkIsAuthenticated } = require("../auth/utils");
+const userController = require('../controllers/userController');
+const { checkIsAuthenticated } = require('../auth/utils');
 
-router.get("/:username", checkIsAuthenticated, userController.get_user_profile);
+router.get('/:username', checkIsAuthenticated, userController.get_user_profile);
+
+router.get(
+  '/account/:username/',
+  checkIsAuthenticated,
+  userController.get_user_account
+);
 
 module.exports = router;
